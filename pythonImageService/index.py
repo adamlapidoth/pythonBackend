fb5026978ef7a942d734c28d704c6fbe31e5fb73
Adam Lapidoth <adamlapidoth@gmail.com>
2023-09-03 22:51:14 +0300

    index.py: implemented the upload backend

--- pythonImageService/index.py
+++ pythonImageService/index.py
@@ -1,15 +1,25 @@
-import tornado.web
 import tornado.ioloop
 
 
-class UploadHandler:
-    pass
+class UploadHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+    def post(self):
+        files = self.request.files["imgFile"]
+        for f in files:
+            with open(f"img/{f.filename}", "wb") as fh:
+                fh.write(f.body)
+            self.write(f"http://localhost:8080/img/{f.filename}")
 
 
-if __name__ == '__main__':
-    app = tornado.web.Application([
-        ("/img/(.*)", tornado.web.StaticFileHandler, {"path": "img"})
-    ])
+if __name__ == "__main__":
+    app = tornado.web.Application(
+        [
+            ("/", UploadHandler),
+            ("/img/(.*)", tornado.web.StaticFileHandler, {"path": "img"}),
+        ]
+    )
 
     port = 8080
     app.listen(port)
